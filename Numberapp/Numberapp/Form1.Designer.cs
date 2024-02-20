4e3f462db29cf8a7ffef58d7d52b3e68bf4c20c9
mm <124887145+MehulBuha@users.noreply.github.com>
2024-02-20 15:46:39 +0530

    Changes

---
+++ Numberapp/Numberapp/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Numberapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBlankSheet = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnUploadImage = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.btnBlankSheet);
+            this.panel1.Location = new System.Drawing.Point(47, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1405, 77);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnBlankSheet
+            // 
+            this.btnBlankSheet.Location = new System.Drawing.Point(1259, 37);
+            this.btnBlankSheet.Name = "btnBlankSheet";
+            this.btnBlankSheet.Size = new System.Drawing.Size(100, 23);
+            this.btnBlankSheet.TabIndex = 0;
+            this.btnBlankSheet.Text = "Sheet1";
+            this.btnBlankSheet.UseVisualStyleBackColor = true;
+            this.btnBlankSheet.Click += new System.EventHandler(this.btnBlankSheet_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Controls.Add(this.btnProcess);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.btnUploadImage);
+            this.panel2.Location = new System.Drawing.Point(61, 187);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1405, 495);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(651, 448);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(100, 23);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(357, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1034, 413);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnUploadImage
+            // 
+            this.btnUploadImage.Location = new System.Drawing.Point(38, 44);
+            this.btnUploadImage.Name = "btnUploadImage";
+            this.btnUploadImage.Size = new System.Drawing.Size(100, 23);
+            this.btnUploadImage.TabIndex = 0;
+            this.btnUploadImage.Text = "UploadImage";
+            this.btnUploadImage.UseVisualStyleBackColor = true;
+            this.btnUploadImage.Click += new System.EventHandler(this.btnUploadImage_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1526, 807);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBlankSheet;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnUploadImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
---
+++ Numberapp/Numberapp/Form1.cs
@@ -0,0 +1,128 @@
+﻿
+using IronOcr;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numberapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBlankSheet_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+
+            // Create a new workbook
+            Microsoft.Office.Interop.Excel.Workbook workbook = excelApp.Workbooks.Add();
+
+            //// Get the active worksheet
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = workbook.ActiveSheet;
+
+            // Add the header
+            worksheet.Cells[1, 1] = "Data";
+
+            // Start from the second row
+            int row = 2;
+
+            // Loop through the specified range of values
+            for (int i = 0; i <= 36; i++)
+            {
+                for (int j = 0; j <= 36; j++)
+                {
+                    for (int k = 0; k <= 36; k++)
+                    {
+                        // Concatenate the values with dashes
+                        string data = $"{i} | {j} | {k}";
+
+                        // Write the data to the cell in Column A and the current row
+                        worksheet.Cells[row, 1] = data;
+
+                        // Move to the next row
+                        row++;
+                    }
+                }
+            }
+
+            //// Save the workbook
+            workbook.SaveAs("BlankExcelSheet.xlsx");
+
+            // Close Excel application
+            excelApp.Quit();
+
+            // Release the resources
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+
+
+            MessageBox.Show("Blank Excel sheet generated successfully.");
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnUploadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            openFileDialog.Title = "Select an Image File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Load the selected image into the PictureBox
+                pictureBox1.Image = new System.Drawing.Bitmap(openFileDialog.FileName);
+
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            // Get the dimensions of the image
+            //int width = pictureBox1.Image.Width;
+            //int height = pictureBox1.Image.Height;
+
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Please upload an image first.");
+                return;
+            }
+            // Read text from the image
+            string extractedText = ReadTextFromImage(pictureBox1.Image);
+
+            // Create a CSV file and write the extracted text to it
+            string csvFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Extracteddata.csv");
+            File.WriteAllText(csvFilePath, extractedText);
+
+
+
+        }
+        private string ReadTextFromImage(Image image)
+        {
+            // Initialize an OCR engine
+            var ocr = new IronTesseract();
+
+            // Read text from the image
+            var result = ocr.Read(image);
+
+            // Return the extracted text
+            return result.Text;
+        }
+    
+    }
+}
+
