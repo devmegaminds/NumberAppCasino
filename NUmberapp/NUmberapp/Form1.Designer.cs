334a167cc75c658677f6d092edfc6a96d6de18f9
mm <124887145+MehulBuha@users.noreply.github.com>
2024-02-29 13:56:24 +0530

    Create blank sheet with series

---
+++ NUmberapp/NUmberapp/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace NUmberapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBlankSheet = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.btnBlankSheet);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(27, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1455, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnBlankSheet
+            // 
+            this.btnBlankSheet.BackColor = System.Drawing.Color.Gray;
+            this.btnBlankSheet.Location = new System.Drawing.Point(1278, 31);
+            this.btnBlankSheet.Name = "btnBlankSheet";
+            this.btnBlankSheet.Size = new System.Drawing.Size(100, 32);
+            this.btnBlankSheet.TabIndex = 1;
+            this.btnBlankSheet.Text = "Sheet";
+            this.btnBlankSheet.UseVisualStyleBackColor = false;
+            this.btnBlankSheet.Click += new System.EventHandler(this.btnBlankSheet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1525, 536);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBlankSheet;
+    }
+}
+
---
+++ NUmberapp/NUmberapp/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NUmberapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const string pipe = "|"; // Define a constant for the dash character
+        private void btnBlankSheet_Click(object sender, EventArgs e)
+        {
+            // Create a SaveFileDialog instance
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            // Set the default file name and extension
+            saveFileDialog1.FileName = "Untitled.csv";
+            saveFileDialog1.DefaultExt = ".csv";
+
+            // Set the initial directory to the Desktop
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            // Display the SaveFileDialog and check if the user clicked the OK button
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var message = "";
+
+                // Loop through the specified range of values
+                for (int i = 0; i <= 36; i++)
+                {
+                    for (int j = 0; j <= 36; j++)
+                    {
+                        for (int k = 0; k <= 36; k++)
+                        {
+                            // Concatenate the values with dashes
+                            message += i.ToString() + pipe + j.ToString() + pipe + k.ToString() + "\n";
+
+                            //message += $"{i.ToString()}-{j.ToString()}-{k.ToString()}\n";
+                        }
+                    }
+                }
+
+                // Get the full file path from the SaveFileDialog
+                string excelFilePath = saveFileDialog1.FileName;
+
+                // Write the message to the selected file
+                File.WriteAllText(excelFilePath, message);
+
+                MessageBox.Show("Excel sheet generated successfully.");
+            }
+        }
+    }
+}
